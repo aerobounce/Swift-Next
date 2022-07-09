0429045c129abd1ab961df48d306583b65227642
aerobounce <10491362+aerobounce@users.noreply.github.com>
2022-07-09 16:27:39 +0900

    Rewrite in bash

---
+++ utils/find_keywords.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -Ceu -o pipefail
+# This script finds files that contains all the specified keywords.
+
+cd ~/Downloads/swift-swift-5.6-RELEASE
+
+find . -type f -print0 |
+    xargs -0 pcregrep -e '\b__consuming\b' -I --files-with-matches |
+    xargs pcregrep -e '\b__owned\b' -I --files-with-matches |
+    xargs pcregrep -e '\b__shared\b' -I --files-with-matches |
+    xargs pcregrep -e '\b__always\b' -I --files-with-matches
