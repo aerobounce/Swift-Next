c6d7e6d97a03361fbe8921cedf298575941023d8
aerobounce <10491362+aerobounce@users.noreply.github.com>
2022-07-28 01:41:36 +0900

    Support `closure-signature-in`

--- tests/syntax_test-TODO.swift
+++ tests/syntax_test-TODO.swift
@@ -7,14 +7,3 @@ func f2(set: (Int) -> Void) {}
 //  ^^ variable.function.swift
 //              ^^ keyword.other.closure-signature-in.swift
 //                 ^^^ variable.function.swift
-
-//
-// labeled-statement
-//
-label: for _ in 0...5 {}
-label: while true {}
-label: repeat {} while true
-
-label: if true { }
-label: switch 0 { default: break }
-label: do { }
