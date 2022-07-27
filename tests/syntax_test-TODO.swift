653028fe943a3e8b31c14ec6df24907f0d8fb1aa
aerobounce <10491362+aerobounce@users.noreply.github.com>
2022-07-28 06:01:48 +0900

    Support value-binding

--- tests/syntax_test-TODO.swift
+++ tests/syntax_test-TODO.swift
@@ -4,6 +4,4 @@ func set(_: Int) -> Int { 0 }
 func f2(set: (Int) -> Void) {}
 
     f2(set: { _ in set(0)})
-//  ^^ variable.function.swift
-//              ^^ keyword.other.closure-signature-in.swift
 //                 ^^^ variable.function.swift
